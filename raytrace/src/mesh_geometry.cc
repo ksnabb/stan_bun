dd6e36401c93b3b5e5be9ab22dcc20c60cd4ee85
Kristoffer <kristoffer.snabb@gmail.com>
2011-04-22 00:39:18 +0300

    mesh geometry modified with some comment,, continue filling the functionality later

--- raytrace/src/mesh_geometry.cc
+++ raytrace/src/mesh_geometry.cc
@@ -3,12 +3,15 @@
 
 mesh_geometry::mesh_geometry (const char *file_name)
 {
+  //copy mesh object loader here and create array of triangles 
 }
   
 double mesh_geometry::hit (const cgmath::ray_3d& ray) const
 {
+  //loop through all triangles and call the hit check which is closest
 }
   
 void mesh_geometry::hit_surface_point (surface_point& pt) const 
 {
+  //something
 }
