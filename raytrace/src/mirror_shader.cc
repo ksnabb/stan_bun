988cb41f02e430027701f61202e510e08f1780d7
Jyry Suvilehto <jsuvileh@Fricka.(none)>
2011-04-25 00:36:45 +0300

    fixed embarassing bug I made in lambert shader and finally added the mirror shader files

--- raytrace/src/mirror_shader.cc
+++ raytrace/src/mirror_shader.cc
@@ -1,24 +1,20 @@
-#include "lambert_shader.hh"
+#include "mirror_shader.hh"
 
 using namespace std;
 using namespace cgmath;
 
-lambert_shader::lambert_shader (const vector_3d& _diffuse)
-  : diffuse (_diffuse)
+mirror_shader::mirror_shader ()
 {}
 
-vector_3d lambert_shader::bsdf (const surface_point& point,
+vector_3d mirror_shader::bsdf (const surface_point& point,
 				const vector_3d&     in_dir,
 				const vector_3d&  /* out_dir */)
 {
   cgmath::vector_3d c(0.0);
-  double cos_theta = -cgmath::dot(in_dir, point.normal);
-  if (cos_theta > 0)
-    c = (cos_theta / M_PI) * diffuse;
   return c;
 }
   
-void lambert_shader::sample_bsdf (const surface_point& point,
+void mirror_shader::sample_bsdf (const surface_point& point,
 				  unsigned             num_samples,
 				  const vector_3d&     out_dir,
 				  bsdf_samples& samples)
@@ -27,5 +23,5 @@ void lambert_shader::sample_bsdf (const surface_point& point,
   matrix_3d R;
   R = rotate (vec(0.0, 0.0, 1.0), point.normal);
   for (unsigned i = 0; i < samples.size(); ++i)
-    samples[i] = bsdf_sample (R * lambert_sample(), diffuse);
+    samples[i] = bsdf_sample (R * out_dir, vec(1.0, 1.0, 1.0));
 }
